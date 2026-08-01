adc9ee68aaa2d5136f0ee39353bd175d660cd5db
Yuhan Cao <yuhanteafrog@gmail.com>
2026-07-31 18:11:01 -0700

    fix publication author expansion

--- assets/js/common.js
+++ assets/js/common.js
@@ -7,5 +7,14 @@ $(document).ready(function() {
         $(this).parent().parent().find(".bibtex.hidden").toggleClass('open');
         $(this).parent().parent().find(".abstract.hidden.open").toggleClass('open');
     });
+    $('button.more-authors').click(function() {
+        var button = $(this);
+        var expanded = button.attr('aria-expanded') === 'true';
+        var hiddenAuthors = button.find('.more-authors-count').text();
+        button.attr('aria-expanded', String(!expanded));
+        button.attr('title', expanded ? 'click to view ' + hiddenAuthors : 'click to hide additional authors');
+        button.find('.more-authors-count').prop('hidden', !expanded);
+        button.find('.more-authors-list').prop('hidden', expanded);
+    });
     $('a').removeClass('waves-effect waves-light');
 });
